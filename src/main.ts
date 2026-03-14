e76e33154a22e15ab3c5d7522cc21190116bbb52
Ngene Samuel Obasi <81524684+cermuel@users.noreply.github.com>
2026-03-14 21:58:52 +0100

    add warts.cermuel.dev to cors

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ origin: ['http://localhost:3000'] });
+  app.enableCors({
+    origin: ['http://localhost:3000', 'https://warts.cermuel.dev'],
+  });
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
